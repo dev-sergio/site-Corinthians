a0ef371441b415b0ef0e807dc337a474895a9fd4
Sergio Junior <sergio.junior@beltsysplus.com.br>
2021-03-13 14:25:57 -0300

    Adicionado função de scroll suave e config do prettier com o eslint

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 import Accordion from './modules/accordion.js';
 import MenuMobile from './modules/menu-mobile.js';
-import ScrollAnima from './modules/scroll-anima.js';
-import TabNav from './modules/tabnav.js';
+import ScrollSuave from './modules/scroll-suave.js';
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"], a[href^="#"]');
+scrollSuave.init();
 
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
-
-const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
-scrollAnima.init();
-
-const nav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
-nav.init();
 
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
