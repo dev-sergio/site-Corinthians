fc7f1d03c8614187f3e03f59700d492e149f0960
Sergio Junior <sergio.ltnj@gmail.com>
2021-03-21 23:07:48 -0300

    correção de merge

--- js/script.js
+++ js/script.js
@@ -27,5 +27,6 @@ accordion.init();
 fetchTitulos('./titulosMasculinoApi.json', '.numeros-mas');
 fetchTitulos('./titulosFemininoApi.json', '.numeros-fem');
 
+
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
